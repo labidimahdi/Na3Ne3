99ec01ca316ca7b195cb6d839f025b66ea486b36
labidi-mahdi <labidimahdi99@gmail.com>
2024-12-03 17:02:27 +0100

    add sidebar admin

--- client/src/components/admin-view/layout.jsx
+++ client/src/components/admin-view/layout.jsx
@@ -1,15 +1,17 @@
 import { Outlet } from "react-router-dom";
-import AdminSidebar from "./sidebar";
+import AdminSideBar from "./sidebar";
 import AdminHeader from "./header";
+import { useState } from "react";
 
 function AdminLayout() {
+    const [openSidebar , setOpenSidebar] = useState(false)
     return(
         <div className="flex min-h-screen w-full">
             {/* admin sidebar */}
-            <AdminSidebar/>
+            <AdminSideBar open={openSidebar} setOpen={setOpenSidebar}/>
             <div className="flex flex-1 flex-col">
                 {/* admin header */}
-                <AdminHeader/>
+                <AdminHeader setOpen={setOpenSidebar}/>
                 <main className="flex-1 flex bg-muted/40 p-4 md:p-6">
                     <Outlet/>
                 </main>
