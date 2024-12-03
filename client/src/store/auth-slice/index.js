a2ab6e8609149a2c83f0d5ad3382dfb74d48b6f7
labidi-mahdi <labidimahdi99@gmail.com>
2024-12-03 16:15:39 +0100

    add the middelware & check-auth

--- client/src/store/auth-slice/index.js
+++ client/src/store/auth-slice/index.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 
 const initialState = {
     isAuthenticated : false,
-    isLoading: false,
+    isLoading: true,
     user : null
 };
 
@@ -27,6 +27,18 @@ export const loginUser = createAsyncThunk('/auth/login',
     }
 );
 
+export const checkAuth = createAsyncThunk('/auth/checkauth', 
+    async() => {
+        const response = await axios.get('http://localhost:5000/api/auth/check-auth', {
+            withCredentials: true,
+            headers : {
+                "Cache-Control":
+            "no-store, no-cache, must-revalidate, proxy-revalidate",
+            }
+        });
+        return response.data;
+    }
+);
 
 const authSlice = createSlice({
     name : 'auth',
@@ -55,6 +67,17 @@ const authSlice = createSlice({
             state.user = action.payload.success ? action.payload.user : null;
             state.isAuthenticated = action.payload.success ;
         }).addCase(loginUser.rejected, (state, action)=> {
+            state.isLoading = false;
+            state.user = null;
+            state.isAuthenticated = false;
+        })
+        .addCase(checkAuth.pending, (state)=> {
+            state.isLoading = true;
+        }).addCase(checkAuth.fulfilled, (state, action)=> {
+            state.isLoading = false;
+            state.user = action.payload.success ? action.payload.user : null;
+            state.isAuthenticated = action.payload.success ;
+        }).addCase(checkAuth.rejected, (state, action)=> {
             state.isLoading = false;
             state.user = null;
             state.isAuthenticated = false;
